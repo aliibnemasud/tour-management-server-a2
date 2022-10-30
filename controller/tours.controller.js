0d4fc1be65b0a4fe4237bdfcb01860084b891bb2
aliibnemasud <mdmasudrony@gmail.com>
2022-10-31 02:29:16 +0600

    get and post added

--- controller/tours.controller.js
+++ controller/tours.controller.js
@@ -1,15 +1,26 @@
 const { getDb } = require("../utils/db/dbConnect")
 
-module.exports.getAllTours = (req, res, next) => {
-    res.send("All Tours")
+module.exports.getAllTours = async (req, res, next) => {
+    const db = getDb();
+    try {
+        const result = await db.collection("tours").find({}).toArray();
+        res.send({success: true, data: result})
+    } catch (error) {
+        console.log(error)
+        next(error)        
+    }    
 }
 
 module.exports.postTour = async (req, res, next) => {
     const db = getDb();
     try {
         const tour = req.body;
-        const result = await db.collection("tours").insertOne(tour);        
-        res.send("Tour Posted Successfully done!")
+        const result = await db.collection("tours").insertOne(tour);
+        if(!result.insertedId){
+           return res.status(400).send({status: false, message: "Something went wrong!"})
+        }                
+        res.send({success: true, message: `Tour Posted Successfully with id: ${result.insertedId}`})
+               
     } catch (error) {
         console.log(error)
         next(error)
